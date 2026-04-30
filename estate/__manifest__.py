0a0129aa12897ec72dacfec506a7e26f822e8652
Lucas <lucas@wappli.be>
2026-04-30 08:40:12 +0200

    [ADD] Real Estate Module

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Real Estate',
+    'application': True,
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml'
+    ]
+}
