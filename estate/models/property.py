8fd2f67cc880b37804f5bbae03a41874303843d0
Lucas <lucas@wappli.be>
2026-05-29 13:55:15 +0200

    [FIX] fixed condition for offer acceptance to use float_compare and float_is_zero as implied in the documentation

--- estate/models/property.py
+++ estate/models/property.py
@@ -1,6 +1,7 @@
 from odoo import fields, models, api
 from odoo.exceptions import UserError, ValidationError
 from datetime import timedelta
+from odoo.tools.float_utils import float_is_zero, float_compare
 
 class Property(models.Model):
     _name = 'estate.property'
@@ -59,8 +60,9 @@ class Property(models.Model):
     @api.constrains('selling_price', 'expected_price')
     def check_price(self):
         for record in self:
-            if record.state not in ['new', 'offer_received'] and record.selling_price < record.expected_price * 0.9:    
-                raise ValidationError("The selling price cannot be lower than 90% of the expected price.")
+            if not float_is_zero(record.selling_price, precision_digits=2):
+                if float_compare(record.selling_price, record.expected_price * 0.9, precision_digits=2) < 0:    
+                    raise ValidationError("The selling price cannot be lower than 90% of the expected price.")
 
     @api.depends('living_area', 'garden_area')
     def _compute_total_area(self):
