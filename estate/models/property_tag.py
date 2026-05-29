c9f5b9bf75d5820c1849df89e760f8692b23d607
Lucas <lucas@wappli.be>
2026-05-29 15:13:04 +0200

    [ADD] Defined default ordering for Property, PropertyType and PropertyTag models

--- estate/models/property_tag.py
+++ estate/models/property_tag.py
@@ -3,6 +3,7 @@ from odoo import fields, models
 class PropertyTag(models.Model):
     _name = 'estate.property.tag'
     _description = 'Real Estate Property Tag'
+    _order = "name asc"
 
     _unique_name = models.Constraint(
         'UNIQUE(name)',
--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -3,6 +3,7 @@ from odoo import fields, models
 class PropertyType(models.Model):
     _name = 'estate.property.type'
     _description = 'Real Estate Property Type'
+    _order = "name asc"
 
     name = fields.Char(string='Type', required=True)
     property_ids = fields.One2many('estate.property', 'type_id', string='Properties')
