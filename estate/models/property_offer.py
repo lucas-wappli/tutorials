6d37ed120f2a9959990474227576c974802332bd
lucas-wappli <lucas@wappli.be>
2026-06-05 12:09:06 +0200

    [ADD] Offers stat button to see offers list by property type

--- estate/models/property_offer.py
+++ estate/models/property_offer.py
@@ -25,6 +25,7 @@ class PropertyOffer(models.Model):
     deadline = fields.Date(string='Deadline', compute='_compute_deadline', inverse='_inverse_deadline', store=True)
     partner_id = fields.Many2one('res.partner', string='Potential Buyer', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
+    property_type_id = fields.Many2one(related='property_id.type_id', string='Property Type', store=True)
 
     @api.depends('validity', 'create_date')
     def _compute_deadline(self):
--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -7,3 +7,21 @@ class PropertyType(models.Model):
 
     name = fields.Char(string='Type', required=True)
     property_ids = fields.One2many('estate.property', 'type_id', string='Properties')
+    offer_ids = fields.One2many('estate.property.offer', 'property_type_id', string='Offers')
+    offer_count = fields.Integer(string='Offer Count', compute='_compute_offer_count')
+
+    def _compute_offer_count(self):
+        for property_type in self:
+            property_type.offer_count = len(property_type.offer_ids)
+
+    def action_view_offers(self):
+        self.ensure_one()
+
+        return {
+            'name': 'Offers',
+            'type': 'ir.actions.act_window',
+            'res_model': 'estate.property.offer',
+            'view_mode': 'list',
+            'context': {'hide_property': False},
+            'domain': [('property_type_id', '=', self.id)],
+        }
